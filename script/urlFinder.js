779c9552843530a516f10d684ee337cb4d6c9ea0
Raf Vandelaer <rafman.rock@hotmail.com>
2015-11-20 20:11:29 +0100

    first steps of nav

---
+++ script/urlFinder.js
@@ -0,0 +1,91 @@
+/**
+ * Created by Raf on 8/11/2015.
+ */
+
+
+function urlFinder() {
+
+    var url = window.location.href;
+    if(url.indexOf("#") > -1) {
+        var n = url.lastIndexOf('#');
+        var result = url.substring(n + 1);
+        return result;
+    }else{
+        return "";
+    }
+    
+}
+
+$(window).on("load", function () {
+    var section = urlFinder();
+    var id;
+    var content = $("#content");
+if(section != "") {
+    var oldActive = $(".active");
+    $(".active").children().each(function () {
+        $(this).hide();
+    });
+
+    $(oldActive).slideUp();
+    $(oldActive).removeClass("active");
+    $(".bold").removeClass("bold");
+
+    content.css("display", "");
+    content.find("section").each(function () {
+        $(this).slideUp("slow");
+    });
+    id = "#" + section;
+
+    //als section leeg is.
+    if(($(id).children().length > 0) == false ) {
+        id = $(id).next("section");
+    }
+    $(id).addClass("active");
+   // alert("find is done.");
+    $(id).show(function () {
+        $(id).slideDown("slow", function () {
+             content.height("initial");
+            //footerPostion();
+           // alert("id+find is done");
+
+            if($(id).attr('id') == "eindwerk"){
+                footerPostion();
+                var height = content.find("div:first-child").height();
+                content.height(height + 20);
+            }
+            else{
+                console.log("other");
+                content.height("initial");
+                footerPostion();
+            }
+
+        });
+    });
+    //TODO: Vet maken van juiste subnav
+    var subnav = $(id).attr("id");
+    $('ul').find("li").each(function () {
+        console.log(subnav);
+      //  console.log("li found: "+ $(this).find("a").attr("link"));
+       if($(this).find("a").attr("link") == subnav){
+           $(this).addClass("bold");
+
+        }
+    });
+
+    if (section == "bestellen") {
+        $(this).css("color", "#E4BA08")
+    } else {
+
+        $("#bestellenLinks").css("color", "#848067")
+    }
+
+
+
+
+    /*pageSizer();
+    footerPostion();*/
+
+}
+
+
+});
